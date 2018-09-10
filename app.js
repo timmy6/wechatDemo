9111d62dfde142c1a08fcc8bc5fcc33ee44b13eb
liuqiming <liuqiminggood@gmail.com>
2018-09-10 23:35:39 +0800

    fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ App({
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        // 发送res.code 到后台换取 openId唯一, sessionKey, unionId
       }
     })
     // 获取用户信息
